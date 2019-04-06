74dfae5df49bb1d44c13409cc200f99fd92c9e48
Eduardo Ramirez <eramirez@redhat.com>
2019-04-06 11:38:32 +0200

    Initial commit

---
+++ getting-started/src/main/java/org/acme/quickstart/GreetingResource.java
@@ -0,0 +1,17 @@
+package org.acme.quickstart;
+
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}

6312956b5b002ea93401de8aeecb7663a7204e65
Eduardo Ramirez <eramirez@redhat.com>
2020-11-03 20:47:45 +0100

    Update Quarkus & Jinq versions and add Tests
    Signed-off-by: Eduardo Ramirez <eramirez@redhat.com>

--- getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,21 +1,40 @@
 package org.acme.quickstart;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.h2.H2DatabaseTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
 @QuarkusTest
+@TestHTTPEndpoint(GreetingResource.class)
+@QuarkusTestResource(H2DatabaseTestResource.class)
 public class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    public void testEmptyContador() {
         given()
-          .when().get("/hello")
+          .when()
+            .get()
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is("Listado:" + System.lineSeparator()));
+    }
+
+    @Test
+    public void testContador1() {
+        given().
+            get( "/Edu" );
+
+        given()
+          .when().get()
+          .then()
+             .statusCode(200)
+             .body(is("Listado:" + System.lineSeparator() + "Edu: 1" ));
     }
 
 }
